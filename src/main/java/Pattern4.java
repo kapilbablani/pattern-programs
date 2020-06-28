a833e63b30abcf0c15819ec3a36f5ddab6a5c662
Kapil Bablani <bablanikapil@gmail.com>
2020-06-28 20:54:27 +0530

    different pattern in java

--- src/main/java/Pattern4.java
+++ src/main/java/Pattern4.java
@@ -2,7 +2,7 @@ package main.java;
 
 public class Pattern4 {
     public static void main(String[] args) {
-        int input = 5;
+        int input=5;
         for(int i=0;i<input;i++) {
             for(int j=1;j<=input-i;j++) {
                 System.out.print(" ");
